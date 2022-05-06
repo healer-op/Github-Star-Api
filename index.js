0998f0bdb6d02a37caa28636613ea53f15412a42
HEALER <65026164+healer-op@users.noreply.github.com>
2022-05-06 10:34:06 +0530

    🌔

---
+++ index.js
@@ -0,0 +1,56 @@
+// 🚀 Npm-Packages 
+const express = require('express')
+const fetch = require("node-fetch");
+const cors = require('cors')
+const app = express()
+
+// ☁️ Utils
+const port = 3000
+app.use(cors());
+
+
+// 🌔 Express Routes
+
+app.get("/", async function (req, res) {
+    res.send("Made By Healer-op <br> http://localhost:3000/api/LocalMiner/Installer/healer-op");
+});
+
+
+app.get("/api/:username/:repo/:name", async function (req, res) {
+    let username = req.params.username;
+    let repo = req.params.repo;
+    let name = req.params.name;
+    fetch(`https://api.github.com/repos/${username}/${repo}/stargazers`)
+        .then(response => {
+            if (!response.ok) {
+                res.send(`check repo link`)
+            }
+            return response.json();
+        })
+        .then(data => {
+            if(data.length > 0){
+                const find = data.find(elem => elem.login === name);
+                if(find.login == name){
+                    res.send(find)
+                }
+            }
+            else{
+                res.send(`false`)
+            }
+        })
+});
+
+
+
+
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log("❌Server crashed❌");
+        console.log("--------------------");
+        console.log(err)
+    } else {
+        console.log(`✅Server started successfully✅`);
+        console.log(`http://localhost:${port}`);
+    }
+})
